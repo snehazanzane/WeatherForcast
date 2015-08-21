a4014abf5cb271c49c2e01ab3e0724ebc0af9328
sneha <sneha@scriptlanes.com>
2015-08-21 19:22:42 +0530

    Some minor bug fixed

--- WeatherForecast/gen/com/example/weatherforecast/R.java
+++ WeatherForecast/gen/com/example/weatherforecast/R.java
@@ -10,45 +10,36 @@ package com.example.weatherforecast;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int grid_bg_gray=0x7f040000;
-        public static final int grid_divider_gray=0x7f040001;
-        public static final int std_blue_color=0x7f040002;
-    }
     public static final class drawable {
         public static final int edit_text_style=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int icon_search=0x7f020002;
-        public static final int search_bar=0x7f020003;
-        public static final int sky=0x7f020004;
-        public static final int sky1=0x7f020005;
-        public static final int sky3=0x7f020006;
+        public static final int sky=0x7f020003;
     }
     public static final class id {
-        public static final int btnGetLocation=0x7f070001;
-        public static final int edtSearchLocation=0x7f070000;
-        public static final int gridDayWeathers=0x7f070003;
-        public static final int imgWeather_WeatherListItem=0x7f070012;
-        public static final int img_WeatherDetails=0x7f070008;
-        public static final int layTlt_WeatherDetails=0x7f070005;
-        public static final int listDayWeathers=0x7f070002;
-        public static final int textView1=0x7f070004;
-        public static final int txtClouds_WeatherDetails=0x7f07000a;
-        public static final int txtClouds_WeatherListItem=0x7f070015;
-        public static final int txtCondition_WeatherListItem=0x7f070014;
-        public static final int txtDate_WeatherDetails=0x7f070006;
-        public static final int txtDate_WeatherListItem=0x7f070013;
-        public static final int txtHumidity_WeatherDetails=0x7f070009;
-        public static final int txtMaxTemp_WeatherListItem=0x7f070016;
-        public static final int txtMinTemp_WeatherListItem=0x7f070017;
-        public static final int txtTpDay_WeatherDetails=0x7f07000c;
-        public static final int txtTpEve_WeatherDetails=0x7f07000f;
-        public static final int txtTpMax_WeatherDetails=0x7f070010;
-        public static final int txtTpMin_WeatherDetails=0x7f070011;
-        public static final int txtTpMor_WeatherDetails=0x7f07000e;
-        public static final int txtTpNight_WeatherDetails=0x7f07000d;
-        public static final int txtType_WeatherDetails=0x7f070007;
-        public static final int txtWindSpeed_WeatherDetails=0x7f07000b;
+        public static final int btnGetLocation=0x7f060001;
+        public static final int edtSearchLocation=0x7f060000;
+        public static final int imgWeather_WeatherListItem=0x7f060011;
+        public static final int img_WeatherDetails=0x7f060007;
+        public static final int layTlt_WeatherDetails=0x7f060004;
+        public static final int listDayWeathers=0x7f060002;
+        public static final int textView1=0x7f060003;
+        public static final int txtClouds_WeatherDetails=0x7f060009;
+        public static final int txtClouds_WeatherListItem=0x7f060014;
+        public static final int txtCondition_WeatherListItem=0x7f060013;
+        public static final int txtDate_WeatherDetails=0x7f060005;
+        public static final int txtDate_WeatherListItem=0x7f060012;
+        public static final int txtHumidity_WeatherDetails=0x7f060008;
+        public static final int txtMaxTemp_WeatherListItem=0x7f060015;
+        public static final int txtMinTemp_WeatherListItem=0x7f060016;
+        public static final int txtTpDay_WeatherDetails=0x7f06000b;
+        public static final int txtTpEve_WeatherDetails=0x7f06000e;
+        public static final int txtTpMax_WeatherDetails=0x7f06000f;
+        public static final int txtTpMin_WeatherDetails=0x7f060010;
+        public static final int txtTpMor_WeatherDetails=0x7f06000d;
+        public static final int txtTpNight_WeatherDetails=0x7f06000c;
+        public static final int txtType_WeatherDetails=0x7f060006;
+        public static final int txtWindSpeed_WeatherDetails=0x7f06000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -56,9 +47,9 @@ public final class R {
         public static final int view_weather_list_item=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int weather_web_key=0x7f050002;
+        public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040001;
+        public static final int weather_web_key=0x7f040002;
     }
     public static final class style {
         /** 
@@ -86,13 +77,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int noAnimTheme=0x7f060004;
-        public static final int spinnerDropDownItemStyle=0x7f060003;
-        public static final int spinnerItemStyle=0x7f060002;
+        public static final int AppTheme=0x7f050001;
+        public static final int noAnimTheme=0x7f050004;
+        public static final int spinnerDropDownItemStyle=0x7f050003;
+        public static final int spinnerItemStyle=0x7f050002;
     }
 }
